f422cd35d79348f556f6879a6ea1c65ad1c77ec5
Shan Dhiviyarajan <prashasoft@gmail.com>
2019-09-25 02:01:24 +0530

    html added to blade

--- resources/views/pages/partner.blade.php
+++ resources/views/pages/partner.blade.php
@@ -1,10 +1,101 @@
 @extends('layouts.master')
-
+@section('title','Partner')
 @section('content')
+    <div class="page-header partner_bg">
+        <div class="page-title">
+            <H2>
+                BECOME A PARTNER
+            </H2>
+        </div>
+        <div class="overlay"></div>
+    </div>
 
-<h1>Partner</h1>
+    <div class="page-content partner-page">
+        <div class="container">
 
 
+            <div class="row justify-content-center">
+                <div class="col-md-7">
+                    <h3 class="text-center mb-4">
+                        <strong>
+                            BECOME A PARTNER TODAY
+                        </strong>
+                    </h3>
+                    <p class="text-left">
+                        What a privilege it is to serve Him during these exciting times! God is accomplishing great and
+                        mighty things, and that’s why I want to personally invite you to become a Covenant Partner so
+                        that,
+                        together, we can fulfill the Great Commission and win the world in His mighty name.
+                        <br><br>
+                        As a partner in this ministry, you will be making a difference in lives around the world, and
+                        what a
+                        privilege it is to serve Him during these exciting times.  hen I minister to the multitudes, my
+                        partners are there with me.
 
+                        <br><br>
+
+                        When I go to the nations through television and crusades, and when we see multiplied tens of
+                        thousands of men, women, and children weeping as they accept Jesus in their lives, you will be
+                        blessed with an eternal harvest for the seed you have sown.
+
+
+                    </p>
+                    <p>
+                        &nbsp;
+                    </p>
+
+
+                </div>
+            </div>
+
+            <div class="donation-btns">
+                <a href="#" class="btn btn-become-partner">
+                    <img src="../public/svg/gift-box.svg" alt=""> Become a monthly <Br>
+                    Partner
+                </a>
+
+                <a href="#" class="btn btn-become-partner">
+                    <img src="../public/svg/gift-box.svg" alt=""> Give a one time <Br>
+                    Donation
+                </a>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-md-9">
+                    <div class="payment-methods">
+                        <h4>
+                            <strong>
+                                Payment methods
+                            </strong>
+                        </h4>
+
+                        <a href="#">
+                            <img src="../public/images/visa.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="../public/images/stripe.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="../public/images/master.png" alt="">
+                        </a>
+
+                        <a href="#">
+                            <img style="width:100px;" src="../public/images/ae.png" alt="">
+                        </a>
+
+                        <a href="#">
+                            <img src="../public/images/apple.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="../public/images/android.png" alt="">
+                        </a>
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 
 @endsection
