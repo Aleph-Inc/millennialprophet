cd37e0c4a1677fbc50f57f9a8339941094137bcc
Shan <prashasoft@gmail.com>
2019-09-26 21:34:53 +0530

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,12 +2,15 @@
     <div class="container">
 
 
-        <svg width="14px" height="10px" viewBox="0 0 14 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <svg width="14px" height="10px" viewBox="0 0 14 10" version="1.1" xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink">
             <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                 <g id="facebook" transform="translate(-1626.000000, -557.000000)" fill="#030104">
                     <g id="12194" transform="translate(1626.000000, 557.000000)">
-                        <path d="M7,7 L5.268,5.484 L0.316,9.729 C0.496,9.896 0.739,10 1.007,10 L12.993,10 C13.26,10 13.502,9.896 13.681,9.729 L8.732,5.484 L7,7 Z" id="Path"></path>
-                        <path d="M13.684,0.271 C13.504,0.103 13.262,0 12.993,0 L1.007,0 C0.74,0 0.498,0.104 0.318,0.273 L7,6 L13.684,0.271 Z" id="Path"></path>
+                        <path d="M7,7 L5.268,5.484 L0.316,9.729 C0.496,9.896 0.739,10 1.007,10 L12.993,10 C13.26,10 13.502,9.896 13.681,9.729 L8.732,5.484 L7,7 Z"
+                              id="Path"></path>
+                        <path d="M13.684,0.271 C13.504,0.103 13.262,0 12.993,0 L1.007,0 C0.74,0 0.498,0.104 0.318,0.273 L7,6 L13.684,0.271 Z"
+                              id="Path"></path>
                         <polygon id="Path" points="0 0.878 0 9.186 4.833 5.079"></polygon>
                         <polygon id="Path" points="9.167 5.079 14 9.186 14 0.875"></polygon>
                     </g>
@@ -32,7 +35,8 @@
 
                         </div>
                         <p>
-                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC
+                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
+                            of classical Latin literature from 45 BC
                         </p>
                     </form>
 
@@ -48,7 +52,7 @@
 
 <!-- footer-->
 <footer>
-    <div class="container mb-4">
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9">
                 <div class="row">
@@ -176,25 +180,22 @@
             </div>
         </div>
     </div>
-
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="copyright">
-                <p class="text-center">
-                    &copy;
-                    2019 Millennial Prophet Ministries &nbsp; &nbsp; &nbsp;| &nbsp;&nbsp; &nbsp;<a href="#">
-                        Termins of use
-                    </a> &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;<a href="#">
-                        Privacy policy
-                    </a>
-                </p>
+    <div class="container bg-white">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copyright">
+                    <p class="text-center">
+                        &copy;
+                        2019 Millennial Prophet Ministries &nbsp; &nbsp; &nbsp;| &nbsp;&nbsp; &nbsp;<a href="#">
+                            Termins of use
+                        </a> &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;<a href="#">
+                            Privacy policy
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-    </div>
-
-
 </footer>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
