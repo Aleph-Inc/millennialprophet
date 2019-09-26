e6ef2815bf257a2cafaa7c7d9fdc6b5fee6555af
Shan <prashasoft@gmail.com>
2019-09-26 13:05:19 +0530

    html updates

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -16,23 +16,20 @@
                 <div class="col-md-12">
                     <ul class="testimony-tabs">
                         <li>
-                            @if($active == 'live')
-                            <a class="active" href="{{ url('/events') }}">
-                            @else
-                            <a class="" href="{{ url('/events') }}">
-                            @endif
+
+                            <a class="{{($active == 'live') ? 'active' : ''}}" href="{{ url('/events') }}">
                                 UPCOMING EVENTS
                             </a>
                         </li>
                         <li class="">
                             @if($active == 'past')
 
-                            <a href="{{ url('/events/past') }}" class="active">
-                            @else
-                            <a class="" href="{{ url('/events/past') }}">
-                            @endif   
-                                PAST EVENTS
-                            </a>
+                                <a href="{{ url('/events/past') }}" class="active">
+                                    @else
+                                         <a class="{{($active == 'live') ? 'active' : ''}}" href="{{ url('/events/past') }}">
+                                            @endif
+                                            PAST EVENTS
+                                        </a>
                         </li>
                     </ul>
                 </div>
@@ -41,52 +38,51 @@
                 <div class="col-md-12">
 
                     @foreach($events as $event )
-                    <a href="#" class="event-item">
-                        <div class="row justify-content-center">
-                            <div class="col-sm-1 p-0">
-                             <div class="event-date">
+                        <a href="#" class="event-item">
+                            <div class="row justify-content-center">
+                                <div class="col-sm-1 p-0">
+                                    <div class="event-date">
                                <span class="date">
                                     {{ $event->date->format('d') }}
 
                                </span>
-                                                            <span class="month">
-                                    {{ $event->date->format('F') }}
+                                        <span class="month">
+                                    {{ $event->date->format('M') }}
 
                               </span>
-                                                            <span class="year">
-                                    {{ $event->date->format('y') }}
+                                        <span class="year">
+                                    {{ $event->date->format('Y') }}
 
                              </span>
+                                    </div>
                                 </div>
+                                <div class="col-sm-3">
+                                    <div class="event-image">
+                                        <img src="{{asset('storage').'/'.$event->image }}" width="261px" height="165px">
+
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="event-title">
+                                        {{ $event->name }}
+                                    </div>
+                                    <div class="event-time">
+                                        {{ $event->time }} @ {{ $event->place }}
+
+                                    </div>
+
+                                    {{--<button class="btn btn-primary btn-event-reg">--}}
+                                    {{--Register--}}
+                                    {{--</button>--}}
+                                    <div class="flag">
+                                        <img src="{{ asset('html/public/images/flag.png') }}" alt="">
+                                    </div>
+                                </div>
+
+
                             </div>
-                            <div class="col-sm-3">
-                                <div class="event-image">
-                                    <img src="{{asset('storage').'/'.$event->image }}" width="261px" height="165px">
 
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="event-title">
-                                {{ $event->name }}
-                                </div>
-                                <div class="event-time">
-                                   {{ $event->time }}  @  {{ $event->place }}
-
-                                </div>
-
-                                <button class="btn btn-primary btn-event-reg">
-                                    Register
-                                </button>
-                                <div class="flag">
-                                    <img src="{{ asset('html/public/images/flag.png') }}" alt="">
-                                </div>
-                            </div>
-
-
-
-                        </div>
-
-                    </a>
+                        </a>
                     @endforeach
 
 
