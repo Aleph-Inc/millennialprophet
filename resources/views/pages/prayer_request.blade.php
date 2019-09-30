247b948c34443f469b21fa6a7ad38e526777e819
Shan <prashasoft@gmail.com>
2019-09-30 20:04:55 +0530

    html updates

--- resources/views/pages/prayer_request.blade.php
+++ resources/views/pages/prayer_request.blade.php
@@ -69,7 +69,7 @@
                             </div>
 
                             <div class="col-md-6">
-                              
+
 
                                 <form action="">
 
--- resources/views/pages/send_testimoney.blade.php
+++ resources/views/pages/send_testimoney.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="page-header page-xl inquiry_bg">
+    <div class="page-header page-xl inquiry_bg mb-0">
 
         <div class="page-header-content">
             <div class="container">
